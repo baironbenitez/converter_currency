31f49d12751f53a65c14b1cbed7d2a8531327e1e
bairon.benitez <ingbaironsis@gmail.com>
2022-10-22 11:25:53 -0500

    Se agrega servicio e interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Currency } from './interfaces/currency.interfac';
+import { CurrencyService } from './services/currency.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'converter_currency';
+export class AppComponent implements OnInit{
+  currencies:Currency[] = [];
+
+  constructor(private currencyService: CurrencyService){}
+
+  ngOnInit(): void {
+    this.currencyService.getCurrencies().subscribe(res=>{
+      this.currencies = res;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormCurrencyComponent } from './components/form-currency/form-currency.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormCurrencyComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    NgSelectModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
